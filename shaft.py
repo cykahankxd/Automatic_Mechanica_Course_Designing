the machine calculation is still under fixing, maybe need the data from shaft

shaft.py
@@ -4,13 +4,39 @@ import numpy as np
 import sys
 from numpy import pi
 
-high_shaft_cal_d = 0.0
-low_shaft_cal_d = 0.0
-high_shaft_design_d = 0.0
-low_shaft_design_d = 0.0
-high_shaft_standard_d = 0.0
-low_shaft_standard_d = 0.0
+high_shaft_cal_d = 0.0  # 高速轴计算直径
+low_shaft_cal_d = 0.0  # 低速轴计算直径
+high_shaft_design_d = 0.0  # 高速轴设计直径
+low_shaft_design_d = 0.0  # 低速轴设计直径
+high_shaft_standard_d = 0.0  # 高速轴标准直径
+low_shaft_standard_d = 0.0  # 低速轴标准直径
 alpha = 20
+d_high_1 = 0.0  # 高速轴各段直径
+d_high_2 = 0.0
+d_high_3 = 0.0
+d_high_4 = 0.0
+d_high_5 = 0.0
+d_high_6 = 0.0
+len_high_1 = 0.0  # 高速轴各段长度
+len_high_2 = 0.0
+len_high_3 = 0.0
+len_high_4 = 0.0
+len_high_5 = 0.0
+len_high_6 = 0.0
+d_low_1 = 0.0  # 低速轴各段直径
+d_low_2 = 0.0
+d_low_3 = 0.0
+d_low_4 = 0.0
+d_low_5 = 0.0
+d_low_6 = 0.0
+len_low_1 = 0.0  # 低速轴各段长度
+len_low_2 = 0.0
+len_low_3 = 0.0
+len_low_4 = 0.0
+len_low_5 = 0.0
+len_low_6 = 0.0
+bearing_cover_high_D = 0.0  # 轴承端盖外径
+bearing_cover_low_D = 0.0
 
 
 def first_design():
@@ -46,17 +72,194 @@ def first_design():
     print("  取标准值为: %.1f" % low_shaft_standard_d)
 
 
+def shaft_length_design():
+
+    global high_shaft_standard_d, len_high_1, len_high_2, len_high_3, len_high_4, len_high_5, len_high_6, \
+        d_high_1, d_high_2, d_high_3, d_high_4, d_high_5, d_high_6, len_low_1, len_low_2, len_low_3, \
+        len_low_4, len_low_5, len_low_6, d_low_1, d_low_2, d_low_3, d_low_4, d_low_5, d_low_6, \
+        bearing_cover_high_D, bearing_cover_low_D
+
+    # 计算螺钉长度
+    nail_len = 0.0
+    bearing_cover_bolt_d = float(config["Machine"]["bearing_cover_bolt_d"])  # 这里是轴承盖直径
+    for i in range(len(standard_bolt[0])):
+        if bearing_cover_bolt_d == standard_bolt[0][i]:
+            nail_len = standard_bolt[1][i] + standard_bolt[2][i]
+
+    # 计算高速轴
+    # 第一段
+    len_high_1 = float(config["Belt"]["L_large_wheel"]) + nail_len
+    d_high_1 = high_shaft_standard_d
+    # 第二段
+    d_high_2 = d_high_1 + 4
+    base_rib_thickness = float(config["Machine"]["base_rib_thickness"])  # 调用一下机壳数据，这里是机座壁厚
+    e = np.ceil(1.2 * bearing_cover_bolt_d)
+    e = e if 5 <= e <= 8 else 8 if e > 8 else 5
+    len_high_2 = base_rib_thickness + e + nail_len
+
+    # 高速轴轴承选用，顺带解决第三段和第六段直径问题
+    bearing_high_T = 0
+    bearing_high_D = 0
+    bearing_ID_high = ""
+
+    d_high_3 = d_high_2
+    for i in range(len(bearing_0_size_series)):
+        if bearing_0_size_series[i][1] > d_high_3:
+            d_high_3 = bearing_0_size_series[i][1]
+            bearing_high_T = bearing_0_size_series[i][3]
+            bearing_high_D = bearing_0_size_series[i][2]
+            bearing_ID_high += str(bearing_0_size_series[i][0])
+            break
+    if bearing_ID_high == "":
+        lastone = len(bearing_0_size_series) - 1
+        d_high_3 = bearing_0_size_series[lastone][1]
+        bearing_high_T = bearing_0_size_series[lastone][3]
+        bearing_high_D = bearing_0_size_series[lastone][2]
+        bearing_ID_high += str(bearing_0_size_series[lastone][0])
+    d_high_6 = d_high_3
+    # 顺带把轴承盖外径给解决了
+    bearing_cover_high_D = bearing_high_D + 5 * bearing_cover_bolt_d
+
+    # 第三段长度
+    delta_3 = 10
+    delta_2 = float(config["Machine"]["delta_2"])
+    len_high_3 = delta_2 + delta_3 + bearing_high_T + 2
+    # 第四段
+    d_high_4 = d_high_3 + 4
+    L_gear_high = b_gear_highspeed if d_high_4 <= b_gear_highspeed <= d_high_4 * 1.5 else np.ceil(d_high_4 * 1.25)
+    len_high_4 = L_gear_high - 2
+    # 第五段
+    a = np.ceil(0.075 * d_high_4 + 1)  # 轴环处轴肩大小
+    d_high_5 = d_high_4 + 2 * a
+    len_high_5 = np.ceil(a * 1.4)
+
+    print("·高速轴计算")
+    print("  根据标准GB/T 276-1994，经计算确定使用深沟球轴承编号为:", bearing_ID_high)
+    print("    其内径为: %.2f" % d_high_3)
+    print("    外径为: %.2f" % bearing_high_D)
+    print("    厚度为: %.2f" % bearing_high_T)
+    print("  根据轴承外径得高速轴轴承盖外径为: %.2f" % bearing_cover_high_D)
+    print("  根据齿轮计算结果，得腹板式齿轮的轴实际参数:")
+    print("    其轴/孔径理论值为: %.2f" % d_high_4)
+    print("    孔长度为: %.2f , 根据《机械设计课程设计》, 轴长向内收缩2~3mm" % L_gear_high)
+    print("  故得出高速轴各段数据如下:")
+    print("  第一节直径: %.2f" % d_high_1)
+    print("       长度: %.2f" % len_high_1)
+    print("  第二节直径: %.2f" % d_high_2)
+    print("       长度: %.2f" % len_high_2)
+    print("  第三节直径: %.2f" % d_high_3)
+    print("       长度: %.2f" % len_high_3)
+    print("  第四节直径: %.2f" % d_high_4)
+    print("       长度: %.2f" % len_high_4)
+    print("  第五节(轴环)直径: %.2f" % d_high_5)
+    print("       长度: %.2f" % len_high_5)
+    print("  第六节直径: %.2f" % d_high_6)
+    print("  (因第六节长度可直接作图得到，故省略)")
+
+    # 低速轴同理
+
+    # 计算所用联轴器
+    coupling_type_low = ""
+    coupling_L_low = 0
+    coupling_L1_low = 0
+    for i in range(len(coupling_standard)):
+        if coupling_standard[i][1] <= low_shaft_standard_d:
+            coupling_type_low = coupling_standard[i][0]
+            coupling_L_low = coupling_standard[i][2]
+            coupling_L1_low = coupling_standard[i][3]
+            break
+    if coupling_type_low == "":
+        lastone = len(coupling_standard[0]) - 1
+        coupling_type_low = coupling_standard[lastone][0]
+        coupling_L_low = coupling_standard[lastone][2]
+        coupling_L1_low = coupling_standard[lastone][3]
+
+    # 第一段
+    len_low_1 = coupling_L1_low
+    d_low_1 = low_shaft_standard_d
+    # 第二段
+    d_low_2 = d_low_1 + 4
+    e = np.ceil(1.2 * bearing_cover_bolt_d)
+    e = e if 5 <= e <= 8 else 8 if e > 8 else 5
+    len_low_2 = base_rib_thickness + e + nail_len
+
+    # 低速轴轴承选用
+    bearing_low_T = 0
+    bearing_low_D = 0
+    bearing_ID_low = ""
+
+    d_low_3 = d_low_2
+    for i in range(len(bearing_0_size_series)):
+        if bearing_0_size_series[i][1] > d_low_3:
+            d_low_3 = bearing_0_size_series[i][1]
+            bearing_low_T = bearing_0_size_series[i][3]
+            bearing_low_D = bearing_0_size_series[i][2]
+            bearing_ID_low += str(bearing_0_size_series[i][0])
+            break
+    if bearing_ID_low == "":
+        lastone = len(bearing_0_size_series) - 1
+        d_low_3 = bearing_0_size_series[lastone][1]
+        bearing_low_T = bearing_0_size_series[lastone][3]
+        bearing_low_D = bearing_0_size_series[lastone][2]
+        bearing_ID_low += str(bearing_0_size_series[lastone][0])
+    d_low_6 = d_low_3
+    bearing_cover_low_D = bearing_low_D + 5 * bearing_cover_bolt_d
+
+    # 第三段长度
+    delta_3 = 10
+    delta_2 = float(config["Machine"]["delta_2"])
+    len_low_3 = delta_2 + delta_3 + bearing_low_T + 2
+    # 第四段
+    d_low_4 = d_low_3 + 4
+    L_gear_low = b_gear_lowspeed if d_low_4 <= b_gear_lowspeed <= d_low_4 * 1.5 else np.ceil(d_low_4 * 1.25)
+    len_low_4 = L_gear_low - 2
+    # 第五段
+    a = np.ceil(0.075 * d_low_4 + 1)  # 轴环处轴肩大小
+    d_low_5 = d_low_4 + 2 * a
+    len_low_5 = np.ceil(a * 1.4)
+
+    print("·低速轴计算")
+    print("  根据标准GB/T 5014-2003，经计算确定使用LX型弹性柱销联轴器编号为:", coupling_type_low)
+    print("    其内径为: %.2f" % d_low_1)
+    print("    其轴孔长度/轴孔小径长度为: %.2f, %.2f" % (coupling_L_low, coupling_L1_low))
+    print("    详细参数详见《机械设计课程设计》")
+    print("  根据标准GB/T 276-1994，经计算确定使用深沟球轴承编号为:", bearing_ID_low)
+    print("    其内径为: %.2f" % d_low_3)
+    print("    外径为: %.2f" % bearing_low_D)
+    print("    厚度为: %.2f" % bearing_low_T)
+    print("  根据轴承外径得高速轴轴承盖外径为: %.2f" % bearing_cover_low_D)
+    print("  根据齿轮计算结果，得腹板式齿轮的轴实际参数:")
+    print("    其轴/孔径理论值为: %.2f" % d_low_4)
+    print("    孔长度为: %.2f , 根据《机械设计课程设计》, 轴长向内收缩2~3mm" % L_gear_low)
+    print("  故得出低速轴各段数据如下:")
+    print("  第一节直径: %.2f" % d_low_1)
+    print("       长度: %.2f" % len_low_1)
+    print("  第二节直径: %.2f" % d_low_2)
+    print("       长度: %.2f" % len_low_2)
+    print("  第三节直径: %.2f" % d_low_3)
+    print("       长度: %.2f" % len_low_3)
+    print("  第四节直径: %.2f" % d_low_4)
+    print("       长度: %.2f" % len_low_4)
+    print("  第五节(轴环)直径: %.2f" % d_low_5)
+    print("       长度: %.2f" % len_low_5)
+    print("  第六节直径: %.2f" % d_low_6)
+    print("  (因第六节长度可直接作图得到，故省略)")
+
+
 def strength_check():
 
     global alpha
 
+    # 高速轴危险截面距离轴承压力中心距离
+    # len_gear_2_bearing =
+
     # 小齿轮处理论所受力
-    peripheral_force_high = 2 * T_highspeed / d_gear_shaft_high  # 圆周力
+    peripheral_force_high = 2 * T_highspeed / d_gear_highspeed  # 圆周力
     radial_force_high = peripheral_force_high * np.tan(np.radians(alpha))  # 根据齿形角计算径向力
 
-
     print("·轴的强度计算")
-    print("  对于直齿圆柱齿轮，理想状况下，减速器轴无轴向力，且轴上载荷方向与大小不变，故此处减速器轴不传递弯矩")
+    print("  对于直齿圆柱齿轮，理想状况下，减速器轴无轴向力，且轴上载荷方向与大小不变，故此处减速器轴向不传递弯矩")
+    print("  对于6000系列深沟球轴承，轴承内壁完全接触轴踢，故压力中心视为轴承中线")
     print("  如下，仅校核危险截面处，即旋转件元件连接处的轴强度")
     print("  1.高速轴部分")
 
@@ -78,16 +281,35 @@ if __name__ == "__main__":
     T_highspeed = float(config["Machine"]["T_highspeed"])
     T_lowspeed = float(config["Machine"]["T_lowspeed"])
     T_output = float(config["Machine"]["T_output"])
+    length_2 = float(config["Machine"]["length_2"])
+    d_gear_highspeed = float(config["Gear"]["d_highspeed"])
+    d_gear_lowspeed = float(config["Gear"]["d_lowspeed"])
     d_gear_shaft_low = float(config["Gear"]["d_gear_shaft_low"])
     d_gear_shaft_high = float(config["Gear"]["d_gear_shaft_high"])
+    b_gear_highspeed = float(config["Gear"]["b_highspeed"])
+    b_gear_lowspeed = float(config["Gear"]["b_lowspeed"])
 
-    # # 将数据输出到可视文件中
-    # output_file = open("Calculated_Data.txt", mode='w+')
-    # temp = sys.stdout
-    # sys.stdout = output_file
+    # 导入标准六角头螺栓长度数据
+    standard_bolt = pd.read_excel('./all_sheets/standard_bolt.xls', sheet_name="Sheet1")
+    train_data = np.array(standard_bolt)
+    standard_bolt = train_data.tolist()
+    # 导入标准零尺寸系列深沟球轴承尺寸数据
+    bearing_0_size_series = pd.read_excel('./all_sheets/bearing_0_size_series.xls', sheet_name="Sheet1")
+    train_data = np.array(bearing_0_size_series)
+    bearing_0_size_series = train_data.tolist()
+    # 导入LX型弹性柱销联轴器尺寸数据
+    coupling_standard = pd.read_excel('./all_sheets/coupling_standard.xls', sheet_name="Sheet1")
+    train_data = np.array(coupling_standard)
+    coupling_standard = train_data.tolist()
+
+    # 将数据输出到可视文件中
+    output_file = open("Calculated_Data.txt", mode='w+')
+    temp = sys.stdout
+    sys.stdout = output_file
     # 程序运行
     first_design()
+    shaft_length_design()
     strength_check()
-    # # 返回输出状态
-    # sys.stdout = temp
-    # output_file.close()
+    # 返回输出状态
+    sys.stdout = temp
+    output_file.close()

whole_machine.py
@@ -2,7 +2,6 @@ import configparser as cp
 import pandas as pd
 import numpy as np
 import sys
-from numpy import pi
 
 base_wall_thickness = 0.0  # 机座壁厚
 hood_wall_thickness = 0.0  # 机盖壁厚
@@ -13,7 +12,6 @@ base_rib_thickness = 0.0  # 机座肋厚
 hood_rib_thickness = 0.0  # 机盖肋厚
 boss_height = 0.0  # 轴承旁凸台高度
 boss_radius = 0.0  # 轴承旁凸台半径
-bearing_cover_diameter = 0.0  # 轴承端盖外径
 foundation_bolt_d = 0.0  # 地脚螺钉直径
 foundation_bolt_n = 0.0  # 地脚螺钉数目
 foundation_bolt_PTH_d = 0.0  # 地脚螺钉通孔直径
@@ -35,24 +33,22 @@ dowel_pin_d = 0.0  # 定位销直径
 bearing_cover_bolt_d = 0.0  # 轴承盖螺钉直径
 seeker_cover_bolt_d = 0.0  # 窥视孔盖螺钉直径
 length_1 = 0.0  # 机体外壁至轴承座断面的距离
+length_2 = 0.0  # 机体内壁至轴承座断面的距离
 delta_1 = 0.0  # 大齿轮顶圆与机体内壁的距离
 delta_2 = 0.0  # 齿轮端面与机体内壁的距离
-
-
-# def shaft_about():
 
 
 def structure_design():
 
     global base_wall_thickness, hood_wall_thickness, base_flange_thickness, hood_flange_thickness, \
-        foundation_flange_thickness, base_rib_thickness, hood_rib_thickness, bearing_cover_diameter, \
+        foundation_flange_thickness, base_rib_thickness, hood_rib_thickness, \
         foundation_bolt_d, foundation_bolt_n, foundation_bolt_seat_d, \
         foundation_bolt_PTH_d, foundation_flange_c1, foundation_flange_c2, \
         bearing_connection_bolt_d, bearing_connection_bolt_seat_d, \
         bearing_connection_bolt_PTH_d, bearing_connection_flange_c1, bearing_connection_flange_c2, \
         shell_connection_bolt_d, shell_connection_bolt_seat_d, \
         shell_connection_bolt_PTH_d, shell_connection_flange_c1, shell_connection_flange_c2, \
-        dowel_pin_d, bearing_cover_bolt_d, seeker_cover_bolt_d, length_1, delta_1, delta_2, boss_radius
+        dowel_pin_d, bearing_cover_bolt_d, seeker_cover_bolt_d, length_1, length_2, delta_1, delta_2, boss_radius
 
     # 初步计算参数
     base_wall_thickness = 0.03 * center_distance if 0.03 * center_distance >= 8 else 8
@@ -124,8 +120,18 @@ def structure_design():
     # 其余参数
     dowel_pin_d = np.ceil(0.7 * shell_connection_bolt_d)
     bearing_cover_bolt_d = np.ceil(0.4 * foundation_bolt_d)
+    for i in range(1, len(standard_bolt[0])):
+        if standard_bolt[0][i] > bearing_cover_bolt_d:
+            bearing_cover_bolt_d = standard_bolt[0][i] if abs(bearing_cover_bolt_d - standard_bolt[0][i]) < \
+                    abs(bearing_cover_bolt_d - standard_bolt[0][i-1]) else standard_bolt[0][i-1]
+            break
     seeker_cover_bolt_d = np.ceil(0.3 * foundation_bolt_d)
+    for i in range(1, len(standard_bolt[0])):
+        if standard_bolt[0][i] > seeker_cover_bolt_d:
+            seeker_cover_bolt_d = standard_bolt[0][i] if abs(seeker_cover_bolt_d - standard_bolt[0][i]) < \
+                    abs(seeker_cover_bolt_d - standard_bolt[0][i-1]) else standard_bolt[0][i-1]
     length_1 = shell_connection_flange_c1 + shell_connection_flange_c2 + 6
+    length_2 = base_wall_thickness + length_1
     delta_1 = np.ceil(1.2 * base_wall_thickness)
     delta_2 = base_wall_thickness if 10 <= base_wall_thickness <= 15 else (10 if base_wall_thickness < 10 else 15)
     boss_radius = shell_connection_flange_c2
@@ -137,8 +143,7 @@ def structure_design():
     print("  机座 / 机盖 / 机器底座凸缘厚为: %.2f / %.2f / %.2f" % (base_flange_thickness, hood_wall_thickness,
                                                         foundation_flange_thickness))
     print("  机座 / 机盖肋板厚为: %.2f , %.2f" % (base_rib_thickness, hood_rib_thickness))
-    print("  轴承旁凸台半径为: %.2f" % bearing_cover_bolt_d)
-    print("  轴承盖外径为: %.2f" % boss_radius)
+    print("  轴承旁凸台半径为: %.2f" % boss_radius)
     print("·地脚螺钉尺寸")
     print("  直径: %.2f" % foundation_bolt_d)
     print("  数量: %.2f" % foundation_bolt_n)
@@ -155,21 +160,20 @@ def structure_design():
     print("  通孔直径: %.2f" % shell_connection_bolt_PTH_d)
     print("  沉头座直径: %.2f" % shell_connection_bolt_seat_d)
     print("  底座凸缘尺寸1 / 2: %.2f , %.2f" % (shell_connection_flange_c1, shell_connection_flange_c2))
-    print("·吊环螺钉尺寸")
     print("·其它组件")
     print("  定位销直径: %.2f" % dowel_pin_d)
     print("  轴承盖螺钉直径: %.2f" % bearing_cover_bolt_d)
     print("  窥视孔盖螺钉直径: %.2f" % seeker_cover_bolt_d)
     print("  机体外壁至轴承座端面的距离: %.2f" % length_1)
+    print("  机体内壁至轴承座端面的距离: %.2f" % length_2)
     print("  大齿轮顶圆与机体内壁的距离: %.2f" % delta_1)
     print("  齿轮端面与机体内壁的距离: %.2f" % delta_2)
-    print("------------------机箱结构运算已结束-------------------")
 
 
 def machine_about_output():
 
     global base_wall_thickness, hood_wall_thickness, base_flange_thickness, hood_flange_thickness, \
-        foundation_flange_thickness, base_rib_thickness, hood_rib_thickness, bearing_cover_diameter, \
+        foundation_flange_thickness, base_rib_thickness, hood_rib_thickness, \
         foundation_bolt_d, foundation_bolt_n, foundation_bolt_seat_d, \
         foundation_bolt_PTH_d, foundation_flange_c1, foundation_flange_c2, \
         bearing_connection_bolt_d, bearing_connection_bolt_seat_d, \
@@ -179,36 +183,39 @@ def machine_about_output():
         dowel_pin_d, bearing_cover_bolt_d, seeker_cover_bolt_d, length_1, delta_1, delta_2, boss_radius
 
     config["Machine"]["base_wall_thickness"] = str(base_wall_thickness)
-    config["Machine"]["base_wall_thickness"] = str(hood_wall_thickness)
-    config["Machine"]["base_wall_thickness"] = str(base_flange_thickness)
-    config["Machine"]["base_wall_thickness"] = str(hood_flange_thickness)
-    config["Machine"]["base_wall_thickness"] = str(foundation_flange_thickness)
-    config["Machine"]["base_wall_thickness"] = str(base_rib_thickness)
-    config["Machine"]["base_wall_thickness"] = str(hood_rib_thickness)
-    config["Machine"]["base_wall_thickness"] = str(boss_radius)
-    config["Machine"]["base_wall_thickness"] = str(bearing_cover_diameter)
-    config["Machine"]["base_wall_thickness"] = str(foundation_bolt_d)
-    config["Machine"]["base_wall_thickness"] = str(foundation_bolt_n)
-    config["Machine"]["base_wall_thickness"] = str(foundation_bolt_PTH_d)
-    config["Machine"]["base_wall_thickness"] = str(foundation_bolt_seat_d)
-    config["Machine"]["base_wall_thickness"] = str(foundation_flange_c1)
-    config["Machine"]["base_wall_thickness"] = str(foundation_flange_c2)
-    config["Machine"]["base_wall_thickness"] = str(bearing_connection_bolt_d)
-    config["Machine"]["base_wall_thickness"] = str(bearing_connection_bolt_PTH_d)
-    config["Machine"]["base_wall_thickness"] = str(bearing_connection_bolt_seat_d)
-    config["Machine"]["base_wall_thickness"] = str(bearing_connection_flange_c1)
-    config["Machine"]["base_wall_thickness"] = str(bearing_connection_flange_c2)
-    config["Machine"]["base_wall_thickness"] = str(shell_connection_bolt_d)
-    config["Machine"]["base_wall_thickness"] = str(shell_connection_bolt_PTH_d)
-    config["Machine"]["base_wall_thickness"] = str(shell_connection_bolt_seat_d)
-    config["Machine"]["base_wall_thickness"] = str(shell_connection_flange_c1)
-    config["Machine"]["base_wall_thickness"] = str(shell_connection_flange_c2)
-    config["Machine"]["base_wall_thickness"] = str(dowel_pin_d)
-    config["Machine"]["base_wall_thickness"] = str(bearing_cover_bolt_d)
-    config["Machine"]["base_wall_thickness"] = str(seeker_cover_bolt_d)
-    config["Machine"]["base_wall_thickness"] = str(length_1)
-    config["Machine"]["base_wall_thickness"] = str(delta_1)
-    config["Machine"]["base_wall_thickness"] = str(delta_2)
+    config["Machine"]["hood_wall_thickness"] = str(hood_wall_thickness)
+    config["Machine"]["base_flange_thickness"] = str(base_flange_thickness)
+    config["Machine"]["hood_flange_thickness"] = str(hood_flange_thickness)
+    config["Machine"]["foundation_flange_thickness"] = str(foundation_flange_thickness)
+    config["Machine"]["base_rib_thickness"] = str(base_rib_thickness)
+    config["Machine"]["hood_rib_thickness"] = str(hood_rib_thickness)
+    config["Machine"]["boss_radius"] = str(boss_radius)
+    config["Machine"]["foundation_bolt_d"] = str(foundation_bolt_d)
+    config["Machine"]["foundation_bolt_n"] = str(foundation_bolt_n)
+    config["Machine"]["foundation_bolt_PTH_d"] = str(foundation_bolt_PTH_d)
+    config["Machine"]["foundation_bolt_seat_d"] = str(foundation_bolt_seat_d)
+    config["Machine"]["foundation_flange_c1"] = str(foundation_flange_c1)
+    config["Machine"]["foundation_flange_c2"] = str(foundation_flange_c2)
+    config["Machine"]["bearing_connection_bolt_d"] = str(bearing_connection_bolt_d)
+    config["Machine"]["bearing_connection_bolt_PTH_d"] = str(bearing_connection_bolt_PTH_d)
+    config["Machine"]["bearing_connection_bolt_seat_d"] = str(bearing_connection_bolt_seat_d)
+    config["Machine"]["bearing_connection_flange_c1"] = str(bearing_connection_flange_c1)
+    config["Machine"]["bearing_connection_flange_c2"] = str(bearing_connection_flange_c2)
+    config["Machine"]["shell_connection_bolt_d"] = str(shell_connection_bolt_d)
+    config["Machine"]["shell_connection_bolt_PTH_d"] = str(shell_connection_bolt_PTH_d)
+    config["Machine"]["shell_connection_bolt_seat_d"] = str(shell_connection_bolt_seat_d)
+    config["Machine"]["shell_connection_flange_c1"] = str(shell_connection_flange_c1)
+    config["Machine"]["shell_connection_flange_c2"] = str(shell_connection_flange_c2)
+    config["Machine"]["dowel_pin_d"] = str(dowel_pin_d)
+    config["Machine"]["bearing_cover_bolt_d"] = str(bearing_cover_bolt_d)
+    config["Machine"]["seeker_cover_bolt_d"] = str(seeker_cover_bolt_d)
+    config["Machine"]["length_1"] = str(length_1)
+    config["Machine"]["length_2"] = str(length_2)
+    config["Machine"]["delta_1"] = str(delta_1)
+    config["Machine"]["delta_2"] = str(delta_2)
+
+    with open("./config.ini", "w") as configfile:
+        config.write(configfile)
 
 
 if __name__ == "__main__":
@@ -234,13 +241,20 @@ if __name__ == "__main__":
     Standard_reducer_con_bolt = pd.read_excel('./all_sheets/Standard_reducer_con_bolt.xls', sheet_name="Sheet1")
     train_data = np.array(Standard_reducer_con_bolt)
     Standard_reducer_con_bolt = train_data.tolist()
+    # 导入标准国标六角头螺栓
+    standard_bolt = pd.read_excel('./all_sheets/standard_bolt.xls', sheet_name="Sheet1")
+    train_data = np.array(standard_bolt)
+    standard_bolt = train_data.tolist()
 
-    # # 将数据输出到可视文件中
-    # output_file = open("Calculated_Data.txt", mode='w+')
-    # temp = sys.stdout
-    # sys.stdout = output_file
+    # 将数据输出到可视文件中
+    output_file = open("Calculated_Data.txt", mode='w+')
+    temp = sys.stdout
+    sys.stdout = output_file
     # 程序运行
     structure_design()
-    # # 返回输出状态
-    # sys.stdout = temp
-    # output_file.close()
+    machine_about_output()
+    print("▲以上部分所用螺钉均以 GB/T 5782-2000 及 GB/T 5783-2000 为设计基准，详见《机械设计课程设计》")
+    print("------------------机箱结构运算已结束-------------------")
+    # 返回输出状态
+    sys.stdout = temp
+    output_file.close()